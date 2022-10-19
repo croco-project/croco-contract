df030dc0f86db5de6e043d24d3419c0b1ddd908e
Vassili Kiritsenko <vassili.kiritsenko@gmail.com>
2022-10-19 23:20:19 +0300

    upd

--- contracts/CrocoToken.sol
+++ contracts/CrocoToken.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-// TODO: Uncomment this line to use console.log
-import "hardhat/console.sol";
+//import "hardhat/console.sol";
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -24,20 +23,18 @@ contract CrocoToken is IERC20, ERC20, Ownable {
         string memory symbol
     ) ERC20 (name, symbol) {}
 
-    function mint(address account, uint256 amount) public onlyOwner {
+    function mint(address account, uint256 amount) external onlyOwner {
         _mint(account, amount);
     }
 
-    function setReferralPermils(uint256[] calldata permils) public onlyOwner {
+    function setReferralPermils(uint256[] calldata permils) external onlyOwner {
         referralPermils = permils;
     }
 
     function transferReferral(address from, address to, uint256 amount, address referrer) public returns (bool) {
         if (referralActive) {
-            if (referrer != address(0)) {
-                addReferral(referrer, to);
-            }
-            _bonus = getReferralAmount(to, referrer);
+            address _referrer = addOrGetReferrer(referrer, to);
+            uint256 _bonus = getReferralAmount(to, amount);
             if (_bonus > 0) {
                 transferFrom(REFERRAL_POOL, _referrer, _bonus);
             }
@@ -45,14 +42,15 @@ contract CrocoToken is IERC20, ERC20, Ownable {
         return transferFrom(from, to, amount);
     }
 
-    function getReferralAmount(address to, uint256 amount) returns (uint256) {
+    function getReferralAmount(address to, uint256 amount) public view returns (uint256) {
         uint256 _bonus = 0;
+
         address _referrer = getReferrer(to);
         if (_referrer != address(0)) {
             uint256 _id = getReferredId(_referrer, to);
             if (_id > 0) {
-                _bonusTemp = amount * _referralPermils[_id] / 100000;
-                if (balanceOf(REFERRAL_POOL) > _bonus) {
+                uint256 _bonusTemp = amount * referralPermils[_id] / 100000;
+                if (balanceOf(REFERRAL_POOL) > _bonusTemp) {
                     _bonus = _bonusTemp;
                 }
             }
@@ -60,21 +58,34 @@ contract CrocoToken is IERC20, ERC20, Ownable {
         return _bonus;
     }
 
-    function addReferral(address referrer, address referred) private {
-        uint256 num = totalReferred[referrer]++;
-        referralsIds[referrer][referred] = num + 1;
-    }
 
-    function getReferredId(address referrer, address referred) public returns (uint256) {
+    function getReferredId(address referrer, address referred) public view returns (uint256) {
         return referralsIds[referrer][referred];
     }
 
-    function getReferrer(address referred) public returns (address) {
+    function getReferrer(address referred) public view returns (address) {
         return referrals[referred];
     }
 
-    function getReferredNumber(address referrer) public returns (uint256) {
+    function getReferredNumber(address referrer) public view returns (uint256) {
         return totalReferred[referrer];
     }
 
+    function addOrGetReferrer(address referrer, address referred) public returns (address) {
+        require(referrer != referred, "Can not add self as referrer");
+
+        address _referrer = getReferrer(referred);
+        if (_referrer == referrer) {
+            return _referrer;
+        }
+
+        if (_referrer != address(0)) {
+            require(_referrer != referred, "Referred can not refer its referrer");
+        }
+
+
+        uint256 num = totalReferred[referrer]++;
+        referralsIds[referrer][referred] = num + 1;
+        return referrals[referred];
+    }
 }
--- contracts/CrocoVesting.sol
+++ contracts/CrocoVesting.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-// TODO: Uncomment this line to use console.log
-import "hardhat/console.sol";
+//import "hardhat/console.sol";
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -12,7 +11,7 @@ import "./CrocoToken.sol";
 
 contract CrocoVesting is Ownable {
     using SafeMath for uint256;
-
+    uint256 ONE_MONTH = 4 weeks;
     bool started;
 
     CrocoToken public crocoToken;
@@ -37,6 +36,12 @@ contract CrocoVesting is Ownable {
     uint256 public privateStart;
     uint256 public publicStart;
 
+    uint256 public preSeedUnlock;
+    uint256 public privateUnlock;
+    uint256 public publicUnlock;
+    uint256 public founderUnlock;
+    uint256 public advisorsUnlock;
+
     enum Stage {
         PRESEED,
         PRIVATE,
@@ -49,11 +54,11 @@ contract CrocoVesting is Ownable {
         usdt = _usdt;
     }
 
-    function withdrawToken(IERC20 _token) public onlyOwner {
+    function withdrawToken(IERC20 _token) external onlyOwner {
         _token.transfer(msg.sender, _token.balanceOf(address(this)));
     }
 
-    function stage() public returns (Stage) {
+    function stage() public view returns (Stage) {
         if (!started) {
             return Stage.CLOSED;
         }
@@ -70,7 +75,7 @@ contract CrocoVesting is Ownable {
     }
 
 
-    function currentRoundPrice() public returns (uint256) {
+    function currentRoundPrice() public view returns (uint256) {
         Stage _stage = stage();
         if (_stage == Stage.PUBLIC) {
             return publicPrice;
@@ -102,37 +107,143 @@ contract CrocoVesting is Ownable {
         publicStart = _publicStart;
     }
 
-    function buyToken(uint256 _amount) public {
+    function setUnlockTime(
+        uint256 _preSeedUnlock,
+        uint256 _privateUnlock,
+        uint256 _publicUnlock,
+        uint256 _founderUnlock,
+        uint256 _advisorsUnlock
+    ) public onlyOwner {
+        preSeedUnlock = _preSeedUnlock;
+        privateUnlock = _privateUnlock;
+        publicUnlock = _publicUnlock;
+        founderUnlock = _founderUnlock;
+        advisorsUnlock = _advisorsUnlock;
+    }
+
+    function buyToken(uint256 _amount, address referrer) public {
         Stage _stage = stage();
         require(_stage != Stage.CLOSED, "Token sale is closed");
-        uint256 currentPrice = currentPrice();
+        uint256 currentPrice = currentRoundPrice();
         require(currentPrice > 0, "Current price is invalid");
         uint256 totalPrice = _amount.mul(currentPrice);
         usdt.transferFrom(msg.sender, address(this), totalPrice);
 
+        address _referrer = crocoToken.addOrGetReferrer(referrer, msg.sender);
+        uint256 _bonus = crocoToken.getReferralAmount(msg.sender, _amount);
+
         if (_stage == Stage.PRESEED) {
             preSeedRound[msg.sender].total += _amount;
             preSeedRound[msg.sender].remainder += _amount;
-
-            crocoToken.transferFrom(crocoToken.REFERRAL_POOL, c);
-
+            if (_bonus > 0) {
+                preSeedRound[_referrer].total += _bonus;
+                preSeedRound[_referrer].remainder += _bonus;
+            }
         } else if (_stage == Stage.PRIVATE) {
             privateRound[msg.sender].total += _amount;
             privateRound[msg.sender].remainder += _amount;
+            if (_bonus > 0) {
+                privateRound[_referrer].total += _bonus;
+                privateRound[_referrer].remainder += _bonus;
+            }
         } else if (_stage == Stage.PUBLIC) {
             publicRound[msg.sender].total += _amount;
             publicRound[msg.sender].remainder += _amount;
+            if (_bonus > 0) {
+                publicRound[_referrer].total += _bonus;
+                publicRound[_referrer].remainder += _bonus;
+            }
+        }
+
+        if (_bonus > 0) {
+            crocoToken.transferFrom(crocoToken.REFERRAL_POOL(), address(this), _bonus);
         }
     }
 
-
-    function availableToClaim(address _address) public {
-
+    function getPreSeedAvailable(address _address) public view returns (uint256) {
+        if (block.timestamp < preSeedUnlock) {
+            return 0;
+        }
+        VestingData memory _data = preSeedRound[_address];
+        return getUnlocked(_data, preSeedUnlock);
     }
 
-    function claim() public {
-        uint256 totalPrice = _amount.mul(currentPrice);
-        usdt.transferFrom(msg.sender, address(this), totalPrice);
-        crocoToken.transfer(msg.sender, _amount);
+    function getPrivateAvailable(address _address) public view returns (uint256) {
+        if (block.timestamp < privateUnlock) {
+            return 0;
+        }
+        VestingData memory _data = privateRound[_address];
+        return getUnlocked(_data, privateUnlock);
+    }
+
+    function getPublicAvailable(address _address) public view returns (uint256) {
+        if (block.timestamp < publicUnlock) {
+            return 0;
+        }
+        VestingData memory _data = publicRound[_address];
+        return getUnlocked(_data, publicUnlock);
+    }
+
+    function getAdvisorsAvailable(address _address) public view returns (uint256) {
+        if (block.timestamp < advisorsUnlock) {
+            return 0;
+        }
+        VestingData memory _data = publicRound[_address];
+        return getUnlocked(_data, advisorsUnlock);
+    }
+
+    function getFoundersAvailable(address _address) public view returns (uint256) {
+        if (block.timestamp < founderUnlock) {
+            return 0;
+        }
+        VestingData memory _data = publicRound[_address];
+        return _data.remainder;
+    }
+
+    function getUnlocked(VestingData memory _data, uint256 unlock) private view returns (uint256) {
+        if (_data.remainder == 0) {
+            return 0;
+        }
+        uint256 unlocks = (block.timestamp - unlock) / ONE_MONTH;
+        uint256 unlocked = _data.total * unlocks / 100;
+        if (unlocked >= _data.remainder) {
+            return _data.remainder;
+        }
+        return unlocked;
+    }
+
+    function claimPreSeed() public {
+        uint256 claimableAmount = getPreSeedAvailable(msg.sender);
+        require(claimableAmount > 0, "No available tokens to claim");
+        preSeedRound[msg.sender].remainder -= claimableAmount;
+        crocoToken.transfer(msg.sender, claimableAmount);
+    }
+
+    function claimPrivate() public {
+        uint256 claimableAmount = getPrivateAvailable(msg.sender);
+        require(claimableAmount > 0, "No available tokens to claim");
+        privateRound[msg.sender].remainder -= claimableAmount;
+        crocoToken.transfer(msg.sender, claimableAmount);
+    }
+
+    function claimPublic() public {
+        uint256 claimableAmount = getPublicAvailable(msg.sender);
+        require(claimableAmount > 0, "No available tokens to claim");
+        publicRound[msg.sender].remainder -= claimableAmount;
+        crocoToken.transfer(msg.sender, claimableAmount);
+    }
+
+    function claimAdvisors() public {
+        uint256 claimableAmount = getAdvisorsAvailable(msg.sender);
+        require(claimableAmount > 0, "No available tokens to claim");
+        advisors[msg.sender].remainder -= claimableAmount;
+        crocoToken.transfer(msg.sender, claimableAmount);
+    }
+
+    function claimFounders() public {
+        uint256 claimableAmount = getFoundersAvailable(msg.sender);
+        require(claimableAmount > 0, "No available tokens to claim");
+        founders[msg.sender].remainder -= claimableAmount;
+        crocoToken.transfer(msg.sender, claimableAmount);
     }
 }
